dc9bf5b31ea96c6aabf12efdad64cf8cc9034a80
Simon Holloway <holloway.sy@gmail.com>
2015-04-28 21:59:24 +0100

    added autoloader

---
+++ src/Autoloader.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Mana Autoloader
+ * 
+ * @package    Harmony
+ * @subpackage Mana
+ * @author     Simon Holloway <holloway.sy@gmail.com>
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+namespace Harmony\Mana;
+
+
+
+
+/**
+ * PSR-4 Autoloader
+ * 
+ * @param string $class The fully-qualified class name.
+ * @return void
+ */
+spl_autoload_register(function ($class) {
+
+    // project-specific namespace prefix
+    $prefix = 'Harmony\\Mana\\';
+
+    // base directory for the namespace prefix
+    $base_dir = __DIR__ . '/src/';
+
+    // does the class use the namespace prefix?
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        // no, move to the next registered autoloader
+        return;
+    }
+
+    // get the relative class name
+    $relative_class = substr($class, $len);
+
+    // replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
+});
+
+// Load the functions file to define all functions
+require('functions.php');
